14c3fd1ffa1b7684cee4408ba4b66adee7cdba7c
Charles Weems <charlie.weems@gmail.com>
2017-01-09 15:36:45 -0800

    Add initial service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,16 @@
+var CACHE_NAME = 'building-inspector-cache-v1';
 
+var baseSiteFiles = [
+  './',
+  '/style.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(baseSiteFiles);
+      })
+  );
+});
